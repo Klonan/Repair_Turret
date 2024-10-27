d2f448c536c7b4aaf06ced5fca35ea1cbdfb5a15
Klonan <11986037+Klonan@users.noreply.github.com>
2024-10-27 11:26:55 +0100

    update hooks

--- script/repair_turret.lua
+++ script/repair_turret.lua
@@ -1140,6 +1140,7 @@ lib.events =
   [defines.events.script_raised_built] = on_created_entity,
   [defines.events.script_raised_revive] = on_created_entity,
   [defines.events.on_entity_cloned] = on_created_entity,
+  [defines.events.on_space_platform_built_entity] = on_created_entity,
 
   [defines.events.on_tick] = on_tick,
   [defines.events.on_entity_damaged] = on_entity_damaged,
@@ -1149,6 +1150,7 @@ lib.events =
   [defines.events.script_raised_destroy] = on_entity_removed,
   [defines.events.on_player_mined_entity] = on_entity_removed,
   [defines.events.on_robot_mined_entity] = on_entity_removed,
+  [defines.events.on_space_platform_mined_entity] = on_entity_removed,
 
   [defines.events.on_object_destroyed] = on_object_destroyed,
 
