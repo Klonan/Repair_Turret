fc11dd5fb5e5e251d57427d550b20e57e446475f
Klonan <11986037+Klonan@users.noreply.github.com>
2024-10-27 11:37:11 +0100

    New corpse + micro

--- data/entities/repair_turret/repair_turret.lua
+++ data/entities/repair_turret/repair_turret.lua
@@ -116,13 +116,13 @@ local animation =
 
 turret.name = name
 turret.localised_name = {name}
-turret.localised_description = {name.."-description"}
+turret.localised_description = {name .. "-description", tostring(repair_range)}
 turret.icon = path .. "repairturret64.png"
 turret.icon_size = 64
 turret.icon_mipmaps = 0
 turret.logistics_radius = 2
-turret.logistics_connection_distance = 16
-turret.construction_radius = 32
+turret.logistics_connection_distance = repair_range
+turret.construction_radius = repair_range * 2
 turret.radar_range = 2
 turret.robot_slots_count = 0
 turret.material_slots_count = 1
@@ -150,7 +150,7 @@ turret.charge_approach_distance = 2
 turret.door_animation_down = util.empty_sprite()
 turret.door_animation_up = util.empty_sprite()
 turret.circuit_wire_max_distance = 0
-turret.corpse = "small-remnants"
+turret.corpse = "repair-turret-remnants"
 turret.minable = {result = name, mining_time = 0.5}
 
 local item = util.copy(data.raw.item.roboport)
@@ -653,3 +653,49 @@ data:extend
 {
   projectile
 }
+
+local remnants ={
+  {
+      type = "corpse",
+      name = "repair-turret-remnants",
+      icon = path.."repairturret128.png",
+      icon_size = 128,
+      flags = {"placeable-neutral", "building-direction-8-way", "not-on-map"},
+      subgroup = "transport-remnants",
+      order = "a-k-a",
+      selection_box = {{-1, -1}, {1, 1}},
+      selectable_in_game = false,
+      time_before_removed = 60 * 60 * 15,
+      final_render_layer = "remnants",
+      remove_on_tile_placement = false,
+      animation =
+      {
+        layers =
+        {
+          {
+            filename = path .. "hr-repairturretrem.png",
+            line_length = 1,
+            width = 192,
+            height = 192,
+            frame_count = 1,
+            direction_count = 4,
+            shift = util.by_pixel(0,0),
+            scale = 0.5,
+          },
+          {
+            filename = path .. "hr-repairturretremmsk.png",
+            line_length = 1,
+            width = 128,
+            height = 128,
+            frame_count = 1,
+            direction_count = 4,
+            tint = {g = 1, r = 0, b = 0, a = 0.5},
+            shift = util.by_pixel(0,0),
+            scale = 0.5,
+          }
+        }
+      }
+  },
+}
+
+data:extend(remnants)
